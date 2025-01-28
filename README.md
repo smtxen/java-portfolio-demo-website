ec604d6ee55c91855603a6b4c785b31cfc6f36e2
smtxen <138354447+smtxen@users.noreply.github.com>
2025-01-28 22:00:15 +0545

    Add files via upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# java-portfolio-demo-website
---
+++ script.js
@@ -0,0 +1,11 @@
+let i = 0;
+const txt = ' design and code, and I love doing it.';
+const speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("write-text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
